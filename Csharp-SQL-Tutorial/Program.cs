0a20c943bbf940b25f319bdd876192f6bb4d3258
Michael Walker <walkerrepair255@yahoo.com>
2021-10-01 16:04:45 -0400

    Add project files.

---
+++ Csharp-SQL-Tutorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace Csharp_SQL_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)                                       //SQL connection open and closure example
+        {
+            var connStr = "server=localhost\\sqlexpress;database=PrsDb;trusted_connection=true;";
+            var sqlConn = new SqlConnection(connStr);
+            sqlConn.Open();               //Makes connection
+            if(sqlConn.State != System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection did not open");
+                return;
+            }
+            Console.WriteLine("Connection opened!");
+
+
+            // continue sql here
+
+
+            sqlConn.Close();
+            if(sqlConn.State != System.Data.ConnectionState.Closed)
+            {
+                Console.WriteLine("Connection closure failed!!");
+                return;
+            }
+            Console.WriteLine("Connection closure success!!");
+        }
+    }
+}
