a5c8d873fc3ba7638c790420607482b10eae3f70
Michael Walker <walkerrepair255@yahoo.com>
2021-10-04 11:20:49 -0400

    created a student class to save on some lines and reuse some code aka if a client want me to munipulate the data again at a later date.

--- Csharp-SQL-Tutorial/Program.cs
+++ Csharp-SQL-Tutorial/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
+using System.Collections.Generic;
 
 namespace Csharp_SQL_Tutorial
 {
@@ -22,22 +23,29 @@ namespace Csharp_SQL_Tutorial
             var sql = "Select * from Student;";
             var cmd = new SqlCommand(sql, sqlConn);
             var reader = cmd.ExecuteReader();
+            var students = new List<Student>();
             while (reader.Read())
             {   //starts at type in sql / becomes an object in the pipeline / must be turned into the data type desired
-                var Id = Convert.ToInt32(reader["Id"]);
-                var Firstname = reader["Firstname"].ToString();
-                var Lastname = Convert.ToString(reader["Lastname"]);
-                var Statecode = reader["Statecode"].ToString();
+                var student = new Student();
+                student.Id = Convert.ToInt32(reader["Id"]);
+                student.Firstname = reader["Firstname"].ToString();
+                student.Lastname = Convert.ToString(reader["Lastname"]);
+                student.StateCode = reader["Statecode"].ToString();
                 //This Column is allow to be null so it needs to be checked and handled
-                var SAT = reader["SAT"].Equals(DBNull.Value)
+                student.SAT = reader["SAT"].Equals(DBNull.Value)
                     //if true its a nullable int
                     ? (int?)null
                     //if false make the now object an int for C#
                     : Convert.ToInt32(reader["SAT"]);
-                var GPA = Convert.ToDecimal(reader["GPA"]);
-                var Message = $"{Id} | {Firstname} | {Lastname} | {Statecode}" +
-                            $" | {(SAT != null ? SAT : "NULL")} | {GPA}";
-                Console.WriteLine(Message);
+                student.GPA = Convert.ToDecimal(reader["GPA"]);
+                student.MajorId = reader["MajorId"].Equals(DBNull.Value)
+                    //if true its a nullable int
+                    ? (int?)null
+                    //if false make the now object an int for C#
+                    : Convert.ToInt32(reader["MajorId"]);
+
+                Console.WriteLine(student);
+                students.Add(student);
             }
             reader.Close();
 
@@ -49,6 +57,8 @@ namespace Csharp_SQL_Tutorial
                 return;
             }
             Console.WriteLine("Connection closure success!!");
+
+
         }
     }
 }
