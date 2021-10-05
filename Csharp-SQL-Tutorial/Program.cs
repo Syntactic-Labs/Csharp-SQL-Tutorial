a2e7952371d470db8d7b3641c3942a0dd5f39b1a
Michael Walker <walkerrepair255@yahoo.com>
2021-10-05 11:03:15 -0400

    SQL interactions Change and Remove. Also add a small level of security Declaring  and using cmd.Parameters.AddWithValue ( )

--- Csharp-SQL-Tutorial/Program.cs
+++ Csharp-SQL-Tutorial/Program.cs
@@ -7,87 +7,105 @@ namespace Csharp_SQL_Tutorial
 {
     class Program
     {
-        static void Main(string[] args)                                    /*SQL connection open and closure example*/
-        {
-            /*create and open connection*/
-            var connectionString
-                = "server=localhost\\sqlexpress;database=EdDb;trusted_connection=true;";
-        var connection = new Connection(connectionString);
-            connection.Open();
-            //pull data from the MajorsController
-            var majorsCtrl = new MajorsController(connection);
-            var major = majorsCtrl.GetByPk(1);
-            Console.WriteLine(major);
-            major = majorsCtrl.GetByPk(11111111);
-            Console.WriteLine(major);
-            var majors = majorsCtrl.GetAll();
-            foreach (var maj in majors)
+		static void Main(string[] args)
+		{
+			var connectionString
+				= "server=localhost\\sqlexpress;database=EdDb;trusted_connection=true;";
+			var connection = new Connection(connectionString);
+			connection.Open();
+
+			var majorsCtrl = new MajorsController(connection);
+
+            var newMajor = new Major()
             {
-                Console.WriteLine(maj);
-            }
-            /*Close the Connection*/
-
-            connection.Close();
-        }
-       
-        
-        
-        //seems to block all code below
-        static void X(){           //places the address in a variable to use in our code
-            var connStr = "server=localhost\\sqlexpress;database=EdDb;trusted_connection=true;";
-            //creates connection  (variable containing the address)
-            var sqlConn = new SqlConnection(connStr);
-            //Opens connection
-            sqlConn.Open();               
-            if(sqlConn.State != System.Data.ConnectionState.Open)
+                Id = 0,
+                Code = "UWBW",
+                Description = "Basket Weaving - Underwater",
+                MinSAT = 1590
+            };
+            var rowsAffected = majorsCtrl.Create(newMajor);
+            if (rowsAffected != 1)
             {
-                Console.WriteLine("Connection did not open");
-                return;
+                Console.WriteLine("Create failed!");
             }
-            Console.WriteLine("Connection opened!");
-            
-            //SQL CODE!!   we are placing all the the query data in the student class
-            var sql = "Select * from Student where GPA between 2.5 and 3.5 order by sat;";
-            var cmd = new SqlCommand(sql, sqlConn);
-            var reader = cmd.ExecuteReader();
-            var students = new List<Student>();
-            while (reader.Read())
-            {   //starts at type in sql / becomes an object in the pipeline / must be turned into the data type desired
-                var student = new Student();
-                student.Id = Convert.ToInt32(reader["Id"]);
-                student.Firstname = reader["Firstname"].ToString();
-                student.Lastname = Convert.ToString(reader["Lastname"]);
-                student.StateCode = reader["Statecode"].ToString();
-                //This Column is allow to be null so it needs to be checked and handled
-                student.SAT = reader["SAT"].Equals(DBNull.Value)
-                    //if true its a nullable int
-                    ? (int?)null
-                    //if false make the now object an int for C#
-                    : Convert.ToInt32(reader["SAT"]);
-                student.GPA = Convert.ToDecimal(reader["GPA"]);
-                student.MajorId = reader["MajorId"].Equals(DBNull.Value)
-                    //if true its a nullable int
-                    ? (int?)null
-                    //if false make the now object an int for C#
-                    : Convert.ToInt32(reader["MajorId"]);
 
-                Console.WriteLine(student);
-                students.Add(student);
-                
-            }
-            reader.Close();
 
-            //ALWAYS close the connection so others can use it.
-            sqlConn.Close();
-            if(sqlConn.State != System.Data.ConnectionState.Closed)
-            {
-                Console.WriteLine("Connection closure failed!!");
-                return;
-            }
-            Console.WriteLine("Connection closure success!!");
+            var major = majorsCtrl.GetByPk(18);
+			rowsAffected = majorsCtrl.Remove(major.Id);
+			
+			Console.WriteLine(major);
+            //major = majorsCtrl.GetByPk(111111);
+			//Console.WriteLine(major);
 
-        }
-    }
+
+			var majors = majorsCtrl.GetAll();
+			foreach (var maj in majors)
+			{
+				Console.WriteLine(maj);
+			}
+
+			connection.Close();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		static void X()
+		{
+			var connStr = "server=localhost\\sqlexpress;database=EdDb;trusted_connection=true;";
+			var sqlConn = new SqlConnection(connStr);
+			sqlConn.Open();
+			if (sqlConn.State != System.Data.ConnectionState.Open)
+			{
+				Console.WriteLine("Connection did not open");
+				return;
+			}
+			Console.WriteLine("Connection opened.");
+
+			var sql = " Select * from Student " +
+						" where gpa between 2.5 and 3.5 " +
+						" order by sat desc;";
+			var cmd = new SqlCommand(sql, sqlConn);
+			var reader = cmd.ExecuteReader();
+			var students = new List<Student>();
+			while (reader.Read())
+			{
+				var student = new Student();
+				student.Id = Convert.ToInt32(reader["Id"]);
+				student.Firstname = reader["Firstname"].ToString();
+				student.Lastname = Convert.ToString(reader["Lastname"]);
+				student.StateCode = reader["StateCode"].ToString();
+				student.SAT = reader["SAT"].Equals(DBNull.Value)
+					? (int?)null
+					: Convert.ToInt32(reader["SAT"]);
+				student.GPA = Convert.ToDecimal(reader["GPA"]);
+				student.MajorId = reader["MajorId"].Equals(DBNull.Value)
+					? (int?)null
+					: Convert.ToInt32(reader["MajorId"]);
+				Console.WriteLine(student);
+				students.Add(student);
+			}
+			reader.Close();
+			sqlConn.Close();
+
+
+		}
+	}
 }
 
 
